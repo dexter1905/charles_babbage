ba8df747117e03eb01a2d7229e35196315a00f6c
Debaditya Sen <debadityasen17@gmail.com>
2021-05-17 17:29:06 +0530

    Weather App : Sample

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,58 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { noop } from 'rxjs/internal-compatibility';
+import { of } from 'rxjs/internal/observable/of';
+
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { CityComponent } from './components/city/city.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MaterialModule } from './material.module';
+import { WeatherService } from './services/weather.service';
+
+class WeatherServiceStub {
+  searchLocation() { return of(noop()); }
+  getCityDetails() { return of(noop()); }
+}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let service: WeatherService;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SearchComponent,
+        CityComponent
       ],
+      imports: [
+        MaterialModule,
+        HttpClientModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        WeatherService
+      ]
     }).compileComponents();
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'WeatherApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('WeatherApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    service = fixture.debugElement.injector.get(WeatherService);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('WeatherApp app is running!');
+  }));
+  it('should render title in a text-mid class', async(() => {
+    const titleEl = fixture.debugElement.query(By.css('.text-mid')).nativeElement;
+    expect(titleEl.textContent).toContain('My Weather App');
+  }));
+  it('should call weatherService.getCityDetails when getCityDetails() is called', () => {
+   const mockCityid = 615702;
+   spyOn(service, 'getCityDetails').and.callThrough();
+
+   component.getCityDetails(mockCityid);
+   expect(service.getCityDetails).toHaveBeenCalledWith(mockCityid);
   });
 });
